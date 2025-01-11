89b0785bfab47c6588e097c2cea879fe344ef9e8
KrystofKavalir <krystof.kavalir@gmail.com>
2025-01-11 17:33:54 +0100

    docs + drobne upravy header

--- server.js
+++ server.js
@@ -28,15 +28,16 @@ const users = []
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(flash())
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }))
+
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride("_method"))
-
 app.use('/Style', express.static(path.join(__dirname, 'style')));
 app.use('/Img', express.static(path.join(__dirname, 'Img')));
 
