2e788c3d159cf11ad0907141fda9049e211202b1
KrystofKavalir <krystof.kavalir@gmail.com>
2025-06-10 15:38:06 +0200

    deployment works?

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-    host: "db", // změna z "localhost" na "db"
-    user: "root",
-    password: "root",
-    database: "azt"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 connection.connect(function(error) {
